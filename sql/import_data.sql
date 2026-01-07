8493a46d218d914e7d740786fc0d58b734e7e545
Mayles <oukacha.mayles@ynov.com>
2026-01-07 21:52:51 +0100

    Modification de import_data

--- sql/import_data.sql
+++ sql/import_data.sql
@@ -9,3 +9,33 @@ CREATE TABLE vehicule_import (
     etat TEXT,
     localisation TEXT
 );
+
+
+INSERT INTO station (ville)
+SELECT DISTINCT localisation
+FROM vehicule_import
+WHERE localisation NOT IN (SELECT ville FROM station);
+
+INSERT INTO vehicule (
+    id_vehicule,
+    marque,
+    modele,
+    annee,
+    energie,
+    autonomie_km,
+    immatriculation,
+    statut,
+    id_station
+)
+SELECT
+    vi.id,
+    vi.marque,
+    vi.modele,
+    vi.annee,
+    vi.energie,
+    vi.autonomie_km,
+    vi.immatriculation,
+    vi.etat,
+    s.id_station
+FROM vehicule_import vi
+JOIN station s ON s.ville = vi.localisation;
