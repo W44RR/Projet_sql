db58acc69ecce391f84f8b453015f28df2c4924a
Mayles <oukacha.mayles@ynov.com>
2026-01-07 16:54:28 +0100

    Permet d'importer les data pour la db

---
+++ sql/import_data.sql
@@ -0,0 +1,11 @@
+CREATE TABLE vehicule_import (
+    id INT,
+    marque TEXT,
+    modele TEXT,
+    annee INT,
+    energie TEXT,
+    autonomie_km INT,
+    immatriculation TEXT,
+    etat TEXT,
+    localisation TEXT
+);
